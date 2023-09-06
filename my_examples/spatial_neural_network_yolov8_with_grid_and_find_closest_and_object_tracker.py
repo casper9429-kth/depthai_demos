44b974e0581e87981043c157892b528a9b8b7245
Kurreman <korayamku@gmail.com>
2023-09-06 16:09:45 +0200

    :construction: imported own fork classifier

--- my_examples/spatial_neural_network_yolov8_with_grid_and_find_closest_and_object_tracker.py
+++ my_examples/spatial_neural_network_yolov8_with_grid_and_find_closest_and_object_tracker.py
@@ -29,6 +29,9 @@ curr_path = Path(__file__).resolve().parent
 configPath = str(curr_path) + '/models/result_low_res/yolov8n_coco.json'
 nnPath = str(curr_path) + '/models/result_low_res/yolov8n_coco_openvino_2022.1_6shave.blob'
 
+#Own fork model
+configPath = str(curr_path) + '/models/fork_classifier/best.json'
+nnPath = str(curr_path) + '/models/fork_classifier/best.blob'
 
 # parse config
 with Path(configPath).open() as f:
@@ -135,7 +138,7 @@ spatialDetectionNetwork.input.setBlocking(False)
 ## Object tracker speficic parameters
 # https://docs.luxonis.com/projects/api/en/latest/components/nodes/object_tracker/ 
 #Get the index of the person class in labels
-ObjectOfInterest = "spoon"
+ObjectOfInterest = "Fork"
 IndexOfInterest = labelMap.index(ObjectOfInterest)
 objectTracker.setDetectionLabelsToTrack([IndexOfInterest]) # Track only person
 # possible tracking types: ZERO_TERM_COLOR_HISTOGRAM, ZERO_TERM_IMAGELESS, SHORT_TERM_IMAGELESS, SHORT_TERM_KCF
@@ -364,9 +367,9 @@ with dai.Device(pipeline,usb2Mode=True) as device:
             cv2.putText(frameOT, f"ID: {[t.id]}", (x1 + 10, y1 + 35), cv2.FONT_HERSHEY_TRIPLEX, 0.5, 255)
             cv2.putText(frameOT, t.status.name, (x1 + 10, y1 + 50), cv2.FONT_HERSHEY_TRIPLEX, 0.5, 255)
             cv2.rectangle(frameOT, (x1, y1), (x2, y2), color, cv2.FONT_HERSHEY_SIMPLEX)
-            # cv2.putText(frameOT, f"X: {int(t.spatialCoordinates.x)} mm", (x1 + 10, y1 + 65), cv2.FONT_HERSHEY_TRIPLEX, 0.5, 255)
-            # cv2.putText(frameOT, f"Y: {int(t.spatialCoordinates.y)} mm", (x1 + 10, y1 + 80), cv2.FONT_HERSHEY_TRIPLEX, 0.5, 255)
-            # cv2.putText(frameOT, f"Z: {int(t.spatialCoordinates.z)} mm", (x1 + 10, y1 + 95), cv2.FONT_HERSHEY_TRIPLEX, 0.5, 255)
+            cv2.putText(frameOT, f"X: {int(t.spatialCoordinates.x)} mm", (x1 + 10, y1 + 65), cv2.FONT_HERSHEY_TRIPLEX, 0.5, 255)
+            cv2.putText(frameOT, f"Y: {int(t.spatialCoordinates.y)} mm", (x1 + 10, y1 + 80), cv2.FONT_HERSHEY_TRIPLEX, 0.5, 255)
+            cv2.putText(frameOT, f"Z: {int(t.spatialCoordinates.z)} mm", (x1 + 10, y1 + 95), cv2.FONT_HERSHEY_TRIPLEX, 0.5, 255)
 
         cv2.putText(frameOT, "NN fps: {:.2f}".format(fps), (2, frameOT.shape[0] - 4), cv2.FONT_HERSHEY_TRIPLEX, 0.4, color)
         cv2.putText(frameOT, "# tracklets: {:.2f}".format(len(trackletsData)), (frameOT.shape[1]-150, (frameOT.shape[0] - 4)), cv2.FONT_HERSHEY_TRIPLEX, 0.4, color)
